37ba7cf2c5a3176f336c05e2c6868ef98d3f1265
alexchogo <chogodad@gmail.com>
2025-12-14 18:08:39 +0300

    latest commit

--- src/app/authpage/AuthForm.tsx
+++ src/app/authpage/AuthForm.tsx
@@ -1,6 +1,6 @@
 
 //FloatingInput with an outlined wrapper and overlapping floating label
-
+"use client"
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
